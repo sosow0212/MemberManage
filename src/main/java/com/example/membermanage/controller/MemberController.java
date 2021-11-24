45da8b1b87a79a96d65e5ac2e57e1043f8f2f507
sosow0212 <sosow0212@naver.com>
2021-11-24 20:47:07 +0900

    상세페이지 처리하기 및 삭제

--- src/main/java/com/example/membermanage/controller/MemberController.java
+++ src/main/java/com/example/membermanage/controller/MemberController.java
@@ -23,16 +23,41 @@ public class MemberController {
         return "memberlist";
     }
 
+
     // 회원정보 등록
     @GetMapping("/member/write")
     public String memberWriteForm() {
         return "memberwrite";
     }
 
+
     // 회원정보 등록 처리
     @PostMapping("/member/writing")
     public String memberWriting(Member member, Model model) {
+
+        // ** 계산 영역 추후에 수정 **
+        // 신청일자는 임의로 설정 - proto1
+        member.setDay("2021-01-01");
+        // 잔여일자도 임의로 설정 - proto1
+        member.setLast(30);
+
         memberService.write(member);
+        return "redirect:/member/list";
+    }
+
+
+    // 회원 상세 페이지
+    @GetMapping("member/view") // localhost:8080/member/view?id=1
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("member", memberService.memberView(id));
+        return "memberview";
+    }
+
+
+    // 특정 게시물 삭제
+    @GetMapping("/member/delete")
+    public String memberDelete(Integer id) {
+        memberService.memberDelete(id);
         return "redirect:/member/list";
     }
 }
