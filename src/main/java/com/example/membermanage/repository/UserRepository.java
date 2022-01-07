1cc692a88882379a390b40073cfb7f0d85146bca
sosow0212 <sosow0212@naver.com>
2022-01-07 20:58:57 +0900

    Security - 2

---
+++ src/main/java/com/example/membermanage/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.membermanage.repository;
+
+import com.example.membermanage.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    User findByUsername(String username);
+
+}
